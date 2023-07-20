6c5ccc79fefa75fb361e2a2ca5144e20fac98ccb
June0619 <jjw5050@hotmail.com>
2023-07-20 13:59:55 +0900

    여백 조정

--- assets/js/lunr/lunr-gr.js
+++ assets/js/lunr/lunr-gr.js
@@ -503,7 +503,7 @@ $(document).ready(function() {
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
               '</h2>'+
               '<div class="archive__item-teaser">'+
-                '<img src="'+store[ref].teaser+'" alt="">'+
+                // '<img src="'+store[ref].teaser+'" alt="">'+
               '</div>'+
               '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
             '</article>'+
